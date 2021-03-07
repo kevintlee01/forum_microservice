8a46dd388865f40cd87cca3717306438551b3f9c
Kevin Lee <kevinlee3618@gmail.com>
2021-03-06 22:30:34 -0800

    Preliminary Commit

---
+++ api-gateway/src/graphql/resolvers/Query/listings.js
@@ -0,0 +1,11 @@
+const listingsResolver = async () => {
+    return [
+        {
+            id: 1,
+            title: "Test",
+            description: "Hello World"
+        }
+    ];
+};
+
+export default listingsResolver;
